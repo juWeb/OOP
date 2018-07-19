fa4550945e1b430df25d1f77726078b0e2b4b980
juWeb <webert.ju57@gmail.com>
2018-07-19 16:18:07 +0200

    test

--- autoloading/index.php
+++ autoloading/index.php
@@ -2,9 +2,9 @@
 
 spl_autoload_register(
     function ($className) {
-        $fileName = __DIR__.DIRECTORY_SEPARATOR.$className.".php";
-        echo "<p>".$fileName."</p>";
-        require_once($fileName); 
+        $className = str_replace('\\', DIRECTORY_SEPARATOR, $className);
+        $fileName = __DIR__.DIRECTORY_SEPARATOR.$className.'.php';
+        require_once $fileName;
     }
 );
 
