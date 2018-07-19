fb45b2eb76005ee5a52c88eed22ad466efcfc1ab
juWeb <webert.ju57@gmail.com>
2018-07-19 15:07:53 +0200

    Add autoloading

--- autoloading/index.php
+++ autoloading/index.php
@@ -1,5 +1,13 @@
 <?php
 
+spl_autoload_register(
+    function ($className) {
+        $fileName = __DIR__.DIRECTORY_SEPARATOR.$className.".php";
+        echo "<p>".$fileName."</p>";
+        require_once($fileName); 
+    }
+);
+
 use Human\Human as Human;
 use Human\Leg;
 
